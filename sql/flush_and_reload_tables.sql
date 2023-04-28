95a3a2a4b16d3e0af1b910f77d572ba2db12c823
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2023-04-28 12:49:21 -0400

    Added rows to db including sql queries for that

--- sql/flush_and_reload_tables.sql
+++ sql/flush_and_reload_tables.sql
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS COURSE  (
  CID VARCHAR(100) NOT NULL PRIMARY KEY,  
  COURSETITLE VARCHAR(100),  
  SEMESTER VARCHAR(100),   
- YEAR DATE NOT NULL
+ YEAR DATE NOT NULL,
+ CREDIT INT UNSIGNED
 ) ;
 CREATE TABLE IF NOT EXISTS ASSIGNMENT (  
  CID VARCHAR(100) NOT NULL PRIMARY KEY,  
@@ -24,3 +25,62 @@ CREATE TABLE IF NOT EXISTS ASSIGNMENT (
  SEMESTER VARCHAR(100),   
  YEAR DATE NOT NULL
 ) ;
+
+INSERT INTO STUDENT VALUES (
+    '48332382',
+    'mfisher',
+    'fhsgdfj#@#&*^473fdg',
+    '2000-03-23',
+    'Mary',
+    'Rose',
+    'Fisher'
+);
+INSERT INTO STUDENT VALUES (
+    '7698560',
+    'jsilverman',
+    'hghd5435sdfg$#@fsgd98',
+    '2001-06-14',
+    'John',
+    'Kand',
+    'Silverman'
+);
+
+INSERT INTO STUDENT VALUES (
+    '5835653',
+    'ccharlotte',
+    'sh34252$#$vfsdf%%',
+    '1999-11-05',
+    'Christine',
+    'Beane',
+    'Charlotte'
+);
+INSERT INTO COURSE VALUES (
+    'CMSC131',
+    'Introduction To Object Oriented Programming I',
+    'SPRING',
+    '2023-01-01',
+    4
+);
+INSERT INTO COURSE VALUES (
+    'CMSC216',
+    'Introduction To System Programming In C',
+    'SPRING',
+    '2023-01-01',
+    4
+);
+INSERT INTO COURSE VALUES (
+    'CMSC351',
+    'Algorithms',
+    'SPRING',
+    '2023-01-01',
+    3
+);
+INSERT INTO COURSE VALUES (
+    'CMSC132',
+    'Introduction To Object Oriented Programming II',
+    'SPRING',
+    '2023-01-01',
+    4
+);
+
+
