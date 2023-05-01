c0c5cae1520d2d31960aea65f8f6b9e2835c1b0a
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2023-05-01 14:03:20 -0400

    Added functions

---
+++ backend.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("./library.php") ;
+
+$action = $_GET['action'];
+$action = strtolower($action);
+switch ($action) {
+    case 'getuser':{
+        
+        $studentpassword = "" ;
+        $studentusername = "" ;
+        
+        if(array_key_exists("username", $_GET)){
+            $studentusername = $_GET['username'];
+        } ;
+
+        if(array_key_exists("password", $_GET)){
+            $studentpassword = $_GET['password'];
+        } ;
+        $result = get_student_infos($username=$studentusername,$password=$studentpassword) ;
+        echo $result ;
+
+    };
+    break;
+    case 'getusers':
+        echo get_all_students();
+        break;
+    case 'putuser':
+        echo "{'result':'TODO:PUTUSER'}";
+        break;
+    default :
+        echo "{'result':'Unknown operation'}";
+    ;
+} ;
+
+
+?>
