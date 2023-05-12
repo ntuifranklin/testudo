3a9788ddcd4a36de9914cc3d91013bf7eaabce3c
jcuff1 <106827688+jcuff1@users.noreply.github.com>
2023-05-11 23:29:36 -0400

    Add Grades Updated

--- androidui/app/src/main/java/com/example/androidfinalprojectcmsc436/AddGradeActivity.kt
+++ androidui/app/src/main/java/com/example/androidfinalprojectcmsc436/AddGradeActivity.kt
@@ -7,31 +7,34 @@ import androidx.appcompat.app.AppCompatActivity
 
 class AddGradeActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var add_grade_button : Button
+    private lateinit var go_back_button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_grades)
         add_grade_button  = findViewById<Button>(R.id.add_grade_button)
         add_grade_button.setOnClickListener(this)
+        go_back_button  = findViewById<Button>(R.id.go_back_button)
+        go_back_button.setOnClickListener(this)
 
     }
 
+    fun goBack() : Unit {
+        finish()
+        overridePendingTransition(R.anim.slide_from_top, 0)
+    }
+
     override fun onClick(v: View?) {
-
-
-        if ( v == null )
-            return
-        /*
-        *  post_course_assignment( assignmentTitle:String, weight : Double, studentuid: String, earnedgrade : Double )
-        * */
-        if  ( v == add_grade_button ) {
+        if ( v != null && v == go_back_button) {
+            goBack()
+        } else if (v != null && v == add_grade_button ) {
             var weight : Double = 0.0
             var studentuid : String = ""
             var earnedgrade : Double = 0.0
             var assignmentTitle : String  = ""
             weight = findViewById<TextView>(R.id.course_ET).text.toString().toDouble()
             studentuid = MainActivity.LOGGED_IN_STUDENT.getUid()
-            earnedgrade = findViewById<TextView>(R.id.grade_ET).text.toString().toDouble()
-            assignmentTitle = findViewById<TextView>(R.id.assignmentTitle).text.toString()
+            earnedgrade = findViewById<TextView>(R.id.percentage_ET).text.toString().toDouble()
+            assignmentTitle = findViewById<TextView>(R.id.title_ET).text.toString()
             var b : Backend = Backend()
             var success : Boolean = b.post_course_assignment(assignmentTitle, weight, studentuid, earnedgrade)
             var m : String = ""
