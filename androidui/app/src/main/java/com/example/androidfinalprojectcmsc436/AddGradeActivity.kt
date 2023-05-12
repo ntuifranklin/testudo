76669f974808c5d4970112355d0a95631f7912d7
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2023-05-11 21:04:55 -0400

    Updated table sql sturcture

--- androidui/app/src/main/java/com/example/androidfinalprojectcmsc436/AddGradeActivity.kt
+++ androidui/app/src/main/java/com/example/androidfinalprojectcmsc436/AddGradeActivity.kt
@@ -1,10 +1,19 @@
 package com.example.androidfinalprojectcmsc436
-
+import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class AddGradeActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_grades)
     }
+
+    override fun onClick(v: View?) {
+
+    }
+
+
+
 }
